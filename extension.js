6003a5c9e32e94e3c5366f83f0d34e8f2a7b0f07
wbyoko <wbagayoko@gmail.com>
2013-04-29 10:42:07 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,11 +1,13 @@
 (function () {
   "use strict";
 
-  function r(f){(/p/.test(document.readyState)?f:setTimeout)(r,9,f);}
+  function r(f,q,c) {
+    var elem = document.querySelector(q);
+    if (elem) f(elem); else setTimeout(r,9,f,q,c?c-1:1e3);
+  }
 
-  r(function () {
-    var iWTemplate = document.querySelector("#box_infowindow\\.html\\#BoxInfowindow div.iw td.basicinfo div.addr > span"),
-      inject = document.createElement('span');
+  r(function (iWTemplate) {
+    var inject = document.createElement('span');
 
     iWTemplate.insertBefore(inject, iWTemplate.firstChild);
 
@@ -13,5 +15,5 @@
     "<span jsvars=\"$event_addr:i.addressLines.join(', ') + ' (' + $event_title + ')'\"></span>" +
     "<span jsvars=\"$event_url:'https://www.google.com/calendar/render?action=TEMPLATE&text='+encodeURIComponent($event_title)+'&location='+encodeURIComponent($event_addr)\"></span>" +
     "<a jsdisplay=\"i.addressLines && i.title\" href=\"#\" jsattrs=\"href:$event_url\" target=\"_blank\">Create Event From Location</a>";
-  });
+  }, "#box_infowindow\\.html\\#BoxInfowindow div.iw td.basicinfo div.addr > span");
 })();
